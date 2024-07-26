4401a2c68bd475d9e4891196e95aec92cb1ace07
Luis <leduardogomez@unicesar.edu.co>
2024-07-26 01:13:57 -0500

    Vista terminada del formulario de registro

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatStepperModule} from '@angular/material/stepper';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    MatStepperModule,
+  ]
+})
+export class MaterialModule { }
--- src/app/public/pages/inscription/inscription.component.ts
+++ src/app/public/pages/inscription/inscription.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
   imports: [
-    PrimeNgModule      
+    PrimeNgModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    
   ],
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent {
+  constructor(private _formBuilder: FormBuilder) {}
+  isLinear = false;
 
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
 }
