eda104a8bec33475b5c7754480cfaecbaac2b634
Luis <leduardogomez@unicesar.edu.co>
2024-08-04 00:22:14 -0500

    Consulta exitosa de los registros guardados en la base de datos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -13,9 +13,20 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(),
     provideAnimations(),
     provideToastr({timeOut: 5000, positionClass: 'toast-top-right'}),
-    { provide: HTTP_INTERCEPTORS, useClass: AddTokenInterceptor, multi: true }
+    provideHttpClient(
+      withInterceptors([
+        (req, next) => {
+          const token = localStorage.getItem('token');
+          if (token) {
+            const clonedRequest = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+            return next(clonedRequest);
+          }
+          return next(req);
+        }
+      ])
+    ),
+
   ]
 };
